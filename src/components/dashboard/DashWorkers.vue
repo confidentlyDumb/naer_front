44af4afe256b4e8489d9dccc2fe768f4d711a0b6
confidentlyDumb <alexandrabushure@gmail.com>
2022-05-08 18:15:54 +0200

    Workers done

--- src/components/dashboard/DashWorkers.vue
+++ src/components/dashboard/DashWorkers.vue
@@ -566,8 +566,8 @@
         >
           <div class="w-1/4 p-2">
             <a 
-              @click="openModal('addTask')"
-              class="flex items-center justify-center px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple w-full"
+              @click="openModal('addWorker')"
+              class="cursor-pointer flex items-center justify-center px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple w-full"
             >
               <span>Add new worker</span>
             </a>
@@ -576,7 +576,7 @@
       </div>
     </div>
     
-        <div id="editWorker" class="hidden fixed inset-0 z-30 items-end bg-black bg-opacity-50 sm:items-center sm:justify-center transition ease-in-out duration-300 opacity-0">
+    <div id="editWorker" class="hidden fixed inset-0 z-30 items-end bg-black bg-opacity-50 sm:items-center sm:justify-center transition ease-in-out duration-300 opacity-0">
 
         <div  class="w-full px-6 py-4 overflow-hidden bg-white rounded-t-lg dark:bg-gray-800 sm:rounded-lg sm:m-4 sm:max-w-xl" role="dialog">
             <!-- Remove header if you don't want a close icon. Use modal body to place modal tile. -->
@@ -991,7 +991,6 @@
       </div>
     </div>
 
-
     <div
       id="editData"
       class="hidden fixed z-50 inset-0 overflow-y-auto items-center justify-center"
@@ -1000,7 +999,7 @@
       aria-modal="true"
     >
       <div
-        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 w-80"
+        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
       >
         <div
           class="fixed inset-0 bg-black bg-opacity-50 transition-opacity"
@@ -1017,16 +1016,29 @@
           class="relative inline-block align-bottom bg-white dark:bg-gray-900 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
         >
           <div class="bg-white dark:bg-gray-900 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <div class="sm:flex sm:items-stretch w-full">
-              
-              <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
-                <h3
+            <div class="sm:flex sm:items-start">
+              <div
+                class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-indigo-800 dark:bg-opacity-40 sm:mx-0 sm:h-10 sm:w-10"
+              >
+
+                <svg class="h-5 text-indigo-600 dark:text-indigo-400"
+                     fill="currentColor"
+                     stroke="currentColor"
+                     aria-hidden="true"
+                     xmlns="http://www.w3.org/2000/svg"
+                     viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                     <path d="M421.7 220.3L188.5 453.4L154.6 419.5L158.1 416H112C103.2 416 96 408.8 96 400V353.9L92.51 357.4C87.78 362.2 84.31 368 82.42 374.4L59.44 452.6L137.6 429.6C143.1 427.7 149.8 424.2 154.6 419.5L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3zM492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75z"/>
+                </svg>
+
+
+              </div>
+                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-80">
+                  <h3
                   class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-200"
                   id="modal-title"
                 >
-                  Edit worker data
+                  Edit data
                 </h3>
-                <div class="mt-2 w-full">
                     <label class="block text-sm mt-4">
                         <span class="text-gray-700 dark:text-gray-400">
                             Firstname
@@ -1053,7 +1065,6 @@
 
                     </label>
                 </div>
-              </div>
             </div>
           </div>
           <div
@@ -1077,6 +1088,83 @@
       </div>
     </div>
 
+
+    <div
+      id="addWorker"
+      class="hidden fixed z-50 inset-0 overflow-y-auto items-center justify-center"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div
+        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+      >
+        <div
+          class="fixed inset-0 bg-black bg-opacity-50 transition-opacity"
+          aria-hidden="true"
+        ></div>
+
+        <span
+          class="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+          >&#8203;</span
+        >
+
+        <div
+          class="relative inline-block align-bottom bg-white dark:bg-gray-900 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+        >
+          <div class="bg-white dark:bg-gray-900 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div class="sm:flex sm:items-start">
+              <div
+                class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-800 dark:bg-opacity-40 sm:mx-0 sm:h-10 sm:w-10"
+              >
+
+                <svg class="h-5 text-green-600 dark:text-green-400"
+                     fill="currentColor"
+                     stroke="currentColor"
+                     aria-hidden="true"
+                     xmlns="http://www.w3.org/2000/svg"
+                     viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                     <path d="M511.6 36.86l-64 415.1c-1.5 9.734-7.375 18.22-15.97 23.05c-4.844 2.719-10.27 4.097-15.68 4.097c-4.188 0-8.319-.8154-12.29-2.472l-122.6-51.1l-50.86 76.29C226.3 508.5 219.8 512 212.8 512C201.3 512 192 502.7 192 491.2v-96.18c0-7.115 2.372-14.03 6.742-19.64L416 96l-293.7 264.3L19.69 317.5C8.438 312.8 .8125 302.2 .0625 289.1s5.469-23.72 16.06-29.77l448-255.1c10.69-6.109 23.88-5.547 34 1.406S513.5 24.72 511.6 36.86z"/>
+                </svg>
+
+              </div>
+                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-80">
+                    <h3
+                    class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-200"
+                    id="modal-title"
+                    >
+                    Add worker to the team
+                    </h3>
+                    <label class="block text-sm mt-4">
+                        <span class="text-gray-700 dark:text-gray-400">
+                            Send an invite to
+                        </span>
+                        <input placeholder="email" type="email" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-input focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
+                    </label>
+                </div>
+            </div>
+          </div>
+          <div
+            class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse dark:bg-gray-900"
+          >
+            <button
+              type="button"
+              class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
+            >
+              Send
+            </button>
+            <button
+              @click="closeModal('addWorker')"
+              type="button"
+              class="flex items-center justify-center px-4 py-2 text-sm font-medium leading-5 text-blue-600 transition-colors duration-150 border border-blue-600 rounded-lg active:bg-transparent hover:border-blue-500 hover:text-blue-500 focus:border-blue-500 focus:text-blue-500 active:text-blue-500 focus:outline-none focus:shadow-outline-gray"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <div
       id="remWorker"
